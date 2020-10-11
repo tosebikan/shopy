43d3760d4cc715ffe8cbb8ef8fc09babb43c12ca
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-10-11 00:30:52 +0000

    add loading component

---
+++ frontend/src/components/Loading.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { render } from "react-dom";
+import Spinner from "react-activity/lib/Spinner";
+import "react-activity/lib/Spinner/Spinner.css";
+import "./loading.css";
+
+function Loading() {
+  return (
+    <div className="loading__container">
+      <Spinner color="#fff" />
+    </div>
+  );
+}
+
+export default Loading;
